6a13f66f829394dcdaa463fa078e18442b4c7a27
SergeyGavr <sergeygavr.94@yandex.ru>
2020-02-09 23:29:12 +0300

    Initial commit, creating Makefile

---
+++ manager/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+echo '<html lang="en">
+<head>
+    <title>App111 </title>
+    <meta charset="UTF-8" />
+</head>
+<body>
+    <h1>Hello, world!</h1>
+</body>
+</html>';
