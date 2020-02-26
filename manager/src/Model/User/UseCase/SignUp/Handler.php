500e4e5b20ea5d5ddc355246b97290297a0c12e3
SergeyGavr <sergeygavr.94@yandex.ru>
2020-02-26 21:42:04 +0300

    Added signup request drafts

---
+++ manager/src/Model/User/UseCase/SignUp/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\SignUp;
+
+
+class Handler
+{
+    public function handle(Command $command): void
+    {
+
+    }
+}
---
+++ manager/tests/Unit/Model/User/SignUp/RequestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\User\SignUp;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest
+{
+    public function testSuccess(): void
+    {
+
+    }
+}
