41ee76e7f8885edfff752e529ebd787a46a23096
SergeyGavr <sergeygavr.94@yandex.ru>
2020-03-02 21:49:54 +0300

    Added signup request usecase

--- manager/tests/Unit/Model/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/SignUp/RequestTest.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Model\User\SignUp;
 
+use App\Model\User\Entity\User\User;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest
 {
     public function testSuccess(): void
     {
+        $user = new User(
+            $email = 'test@app.test',
+            $hash = 'hash'
+        );
 
+        self::assertEquals($email, $user->getEmail());
+        self::assertEquals($hash, $user->getPasswordHash());
     }
 }
